a85582fd0fc965a56b996e125980c2c3819b957e
Helton Sanchez <helton.sanchez@ymail.com>
2022-10-23 20:32:06 -0300

    entrega JS s4-19 adicionada

---
+++ kenzie-m1/atividades-JS/S4-13Atividade-JavaScript-Lista-de-Array-I/script.js
@@ -0,0 +1,92 @@
+/*
+    1. Escreva uma função que recebe um valor do tipo string e com no mínimo 5 caracteres. insira o valor recebido em um array.
+
+    Retorne o array gerado.
+        MAPA LOGICO
+    1. criar uma função com parametro para string
+    2. criar um array com 
+*/
+
+/*
+    2. Escreva uma função que recebe um valor do tipo number. Use o valor recebido para acessar uma posição do seguinte array:
+
+
+    let arr = ["Hermanoteu", "Olonéia", "Migalatéia", "Godart"]⁠
+
+
+    Caso não seja acessado nenhuma posição com o valor recebido,
+    alerte: "Valor não encontrado", caso seja retorne o nome da posição.
+*/
+
+/*
+    3. Crie um array de números, contendo os valores de 1 a 10. Crie uma função que recebe um número inteiro.
+
+    Acesse uma posição do array de números usando o valor recebido. Teste se o valor contido na posição é par ou ímpar.
+
+    Caso seja retorne: "O valor encontrado nesta posição é par",
+    caso contrário retorne: "O valor encontrado nesta posição é ímpar".
+*/
+
+/*
+    4. Dado o array de nomes:
+    ⁠let nomes = ["Pedro", "Rafael", "José"]⁠
+
+    Crie uma função que recebe uma lista de nomes como parâmetro. A função deve verificar qual dos nomes da lista tem a maior quantidade de caracteres e retornar o nome.
+
+    Teste cada posição usando estrutura de repetição, a menos que a lista tenha apenas um valor inserido.
+
+    Use este formato: "O nome x é o maior da lista";
+*/
+
+/*
+    5. Dado o array de números como exemplo:
+    ⁠numeros = [1,4,6,9,11,8]
+
+
+    Escreva uma função que recebe uma lista de números como parâmetro. A função deve realizar a soma dos valores das duas últimas posições da lista e armazenar o valor da soma em uma variável.
+
+    Use o valor obtido para testar se o valor da soma é um múltiplo de 2.
+
+    Se sim, retorne: "A soma é um múltiplo de 2".
+    Caso contrário: "A soma não é um múltiplo de 2"
+*/
+
+/*
+    6. Dado o array de números como exemplo:
+    ⁠numeros = [1,4,6,9,11,8]
+
+    Escreva uma função que recebe uma lista de números como parâmetro. A função deve realizar a soma dos valores das duas primeiras posições da lista e armazenar o valor da soma em uma variável.
+
+    Use o valor obtido para testar se o valor da soma é um múltiplo de 5.
+
+    Se sim, retorne: "A soma é um múltiplo de 5".
+    Caso contrário: "A soma não é um múltiplo de 5"
+*/
+
+/*
+    7. Dado o array de nomes como exemplo:
+    ⁠let nomes = ["Pedro", "Rafael", "José", "Carla", "Maria", "Josef", "Raquel"]⁠
+
+    Escreva uma função que recebe uma lista de nomes como parâmetro, um nome(string) e um valor inteiro(number) para representar a posição.
+
+    Acesse na lista a posição que corresponde ao valor inteiro recebido e armazene o valor contido na posição em uma variável
+
+    Teste se o nome, recebido pela função, é o mesmo, armazenado na variável.
+
+    Lembre-se de normalizar os dados, antes de testar.
+
+    Caso seja, retorne: "Acertei".
+    Caso não seja: "Não é quem eu pensava"
+*/
+
+/*
+    8. Dado dois arrays numéricos como exemplo.
+    ⁠let numeros = [1,2,3,4,5,6,10]
+    ⁠let outrosNumeros = [5,7,9,4,2,3,9]
+
+    Escreva uma função, que recebe duas listas de números como parâmetro. A função deve conseguir identificar, qual dos dois array's é o maior.
+
+    Após identificar o maior array, retorne o último elemento do array.
+
+    Use este formato: "A maior lista é a lista cujo último número é: x"
+*/
